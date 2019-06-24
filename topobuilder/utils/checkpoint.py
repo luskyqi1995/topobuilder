d31c8a8fff087b453185530fc3dc077cc8c93e7a
Jaume Bonet <jaume.bonet@gmail.com>
2019-06-24 13:01:56 +0200

    fix numpy types to json encoding

--- topobuilder/utils/checkpoint.py
+++ topobuilder/utils/checkpoint.py
@@ -14,6 +14,7 @@ import sys
 from types import GeneratorType
 
 # External Libraries
+import numpy as np
 
 # This Library
 import topobuilder.core as TBcore
@@ -29,6 +30,14 @@ class GeneralEncoder(json.JSONEncoder):
             return list(o)
         if isinstance(o, set):
             return list(o)
+        if isinstance(o, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32,
+                          np.int64, np.uint8, np.uint16, np.uint32, np.uint64)):
+            return int(o)
+        elif isinstance(o, (np.float_, np.float16, np.float32, np.float64)):
+            return float(o)
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
+
         return json.JSONEncoder.default(self, o)
 
 
