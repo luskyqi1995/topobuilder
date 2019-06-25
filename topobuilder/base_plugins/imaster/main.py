c12bf0c69db9cd14ba35aa953ab8e41d4a1d05a1
Jaume Bonet <jaume.bonet@gmail.com>
2019-06-25 13:12:01 +0200

    imaster fix

--- topobuilder/base_plugins/imaster/main.py
+++ topobuilder/base_plugins/imaster/main.py
@@ -119,7 +119,7 @@ def case_apply( case: Case,
                          list(zip(cstrs, cends)),
                          list(next(flip) for _ in range(len(CKase.ordered_structures)))))
         print(rules)
-        print(pdb_geometry_from_rules(structure, rules))
+        print(TButil.pdb_geometry_from_rules(structure, rules))
 
         # MASTER search
         createpds = TButil.createPDS(query)
